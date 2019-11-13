af8fb1824e41d593d0a0f888ab0ad0f8ab2509a0
waterval <luukvla@hotmail.com>
2019-11-13 17:27:42 +0100

    removed a console log

--- src/birthdays/birthday-actions.js
+++ src/birthdays/birthday-actions.js
@@ -2,7 +2,6 @@ import axios from "../app/axios";
 
 export async function getBirthdays() {
     const { data } = await axios.get("/api/birthdays");
-    console.log("data: ", data);
     return {
         type: "GET_BIRTHDAYS",
         users: data.users
