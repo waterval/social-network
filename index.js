4a440637eaf27bb693d26ca9f4cce22e583e3295
waterval <luukvla@hotmail.com>
2020-01-10 18:38:26 +0100

    changed origins url for sockets

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
 const server = require("http").Server(app);
-const io = require("socket.io")(server, { origins: "localhost:8080" });
+const io = require("socket.io")(server, {
+    origins: "localhost:8080 social-network-waterval.herokuapp.com:*"
+});
 const compression = require("compression");
 const cookieSession = require("cookie-session");
 const csurf = require("csurf");
